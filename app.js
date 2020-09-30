bbfb99ab48f0c522546aaaf0a5f748bb50a76d08
psayeed1990 <sayeedmondal1412@gmail.com>
2020-09-30 14:12:16 +0600

    added security

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const express = require(express);
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -8,4 +17,58 @@ const productRoutes = require('/routes/productRoutes');
 const categoryRoutes = require('/routes/categoryRoutes');
 const cartRoutes = require('/routes/cartRoutes');
 
-//
+//enable proxy
+app.enable('trust-proxy');
+
+//cors
+app.use(cors());
+app.options('*', cors());
+
+//public static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+app.use('/', (req, res) => res.send('Hello Faddist'));
+
+//set security http header
+app.use(helmet());
+
+//rate limit
+app.use(
+  '*',
+  rateLimit({
+    max: 1000,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many http request, please try again in a hour',
+  })
+);
+
+//body parser
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+//Database request sanitize against nosql injection
+app.use(mongoSanitize());
+
+// Data clean against xss attack
+app.use(xss());
+
+//prevent parameter pollution
+app.use(hpp());
+
+app.use(compression());
+
+//routes
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/categories', categoriesRoutes);
+app.use('/api/v1/carts', cartsRoutes);
+
+// 404
+app.all('*', (req, res, next) => {
+  res.send('Not found');
+});
+
+//export app
+module.exports = app;
