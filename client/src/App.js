9eac4efed226e5a7261c2f4394adce8624b03ed4
psayeed1990 <sayeedmondal1412@gmail.com>
2020-10-03 10:13:00 +0600

    added menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import Navbar from './components/base/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Faddist</h1>
-      <p>A ecommerce shoppinh cart is under development</p>
-    </div>
-  );
-}
+//import base components
+// import Navbar from './components/base/Navbar';
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <h1>Faddist</h1>
+    <p>A ecommerce shoppinh cart is under development</p>
+  </Fragment>
+);
 
 export default App;
--- client/src/components/base/Navbar.js
+++ client/src/components/base/Navbar.js
@@ -1,20 +1,95 @@
-<nav>
-  <div class="nav-wrapper">
-    <a href="" class="brand-logo">
-      Logo
-    </a>
-    <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li>
-        <a href="">sass</a>
-      </li>
-      <li>
-        <a href="">
-          sass <span class="new badge">4</span>
-        </a>
-      </li>
-      <li>
-        <a href="">sass</a>
-      </li>
-    </ul>
-  </div>
-</nav>;
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
+const Navbar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        Open Categories
+      </Button>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <ListItemIcon>
+            <SendIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Makeup" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <DraftsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Cloth" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <InboxIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Bag" />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
+  );
+};
+
+export default Navbar;
